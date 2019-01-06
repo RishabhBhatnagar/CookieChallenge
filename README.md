033cb439b12db296b0550c3d1d62c619249d29ca
RishabhBhatnagar <bhatnagarrishabh4@gmail.com>
2019-01-06 16:01:57 +0530

    added code in readme

--- README.md
+++ README.md
@@ -78,4 +78,43 @@ For each cell in the matrix, an algorithm analogus to boundary fill algorithm fr
       neighbour_count(bottom)  
       // this step will count the nearby cells having chocolate chip.  
   6. return choco_chip_count  
-  ```
+
+
+## Code
+```python
+def neighbour_count(cookie, x, y, choco_chip, m, n):
+    if (x<m and y<n) and (x>=0 and y>=0):
+        if cookie[x][y] == choco_chip:
+            # current chip is chocolate chip.
+            # counting current chip.
+            neighbour_count.count += 1
+            # replacing current cell value to non choco chip value
+            cookie[x][y] = 1-choco_chip
+            
+            # running this algo for neighbouring cells.
+            neighbour_count(cookie, x+1, y,   choco_chip, m, n) # right
+            neighbour_count(cookie, x-1, y,   choco_chip, m, n) # left
+            neighbour_count(cookie, x,   y+1, choco_chip, m, n) # top
+            neighbour_count(cookie, x,   y-1, choco_chip, m, n) # down
+                  
+    return neighbour_count.count
+
+def main(cookie, choco_chip):
+    m, n = len(cookie), len(cookie[0])
+    for i, row in enumerate(cookie):
+        for j, col in enumerate(row):
+            if col == choco_chip:      # checking if curent_cell is a choco_chip
+                neighbour_count.count = 0
+                yield neighbour_count(cookie, i, j, choco_chip, m, n)
+
+
+if __name__ == '__main__':
+    choco_chip = 1
+    cookie_string = """010000
+                       101101
+                       011011
+                       100100
+                       000000"""
+    cookie = [[int(chip) for chip in row.strip()] for row in cookie_string.split('\n')]
+    print(list(main(cookie, choco_chip)))
+```
--- cookie.py
+++ cookie.py
@@ -1,31 +1,35 @@
-cookie_string = """
-                010000
-                101101
-                011011
-                100100
-                000000
-                """
-cookie_rows = [i.strip() for i in cookie_string.split('\n') if i.strip()]
-cookie = []
-for row in cookie_rows:
-    cookie.append([int(chip) for chip in row])
-
-
-def neighbour_count(cookie, x, y, cookie_type, m, n):
+def neighbour_count(cookie, x, y, choco_chip, m, n):
     if (x<m and y<n) and (x>=0 and y>=0):
-        if cookie[x][y] == cookie_type:
+        if cookie[x][y] == choco_chip:
+            # current chip is chocolate chip.
+            # counting current chip.
             neighbour_count.count += 1
-            cookie[x][y] = 1-cookie_type
-            neighbour_count(cookie, x+1, y,   cookie_type, m, n)
-            neighbour_count(cookie, x-1, y,   cookie_type, m, n)
-            neighbour_count(cookie, x,   y+1, cookie_type, m, n)
-            neighbour_count(cookie, x,   y-1, cookie_type, m, n)            
+            # replacing current cell value to non choco chip value
+            cookie[x][y] = 1-choco_chip
+            
+            # running this algo for neighbouring cells.
+            neighbour_count(cookie, x+1, y,   choco_chip, m, n) # right
+            neighbour_count(cookie, x-1, y,   choco_chip, m, n) # left
+            neighbour_count(cookie, x,   y+1, choco_chip, m, n) # top
+            neighbour_count(cookie, x,   y-1, choco_chip, m, n) # down
+                  
     return neighbour_count.count
 
+def main(cookie, choco_chip):
+    m, n = len(cookie), len(cookie[0])
+    for i, row in enumerate(cookie):
+        for j, col in enumerate(row):
+            if col == choco_chip:      # checking if curent_cell is a choco_chip
+                neighbour_count.count = 0
+                yield neighbour_count(cookie, i, j, choco_chip, m, n)
 
-m, n = len(cookie), len(cookie[0])
-for i, row in enumerate(cookie):
-    for j, col in enumerate(row):
-        if col == 1:
-            neighbour_count.count = 0
-            print(neighbour_count(cookie, i, j, 1, m, n))
+
+if __name__ == '__main__':
+    choco_chip = 1
+    cookie_string = """010000
+                       101101
+                       011011
+                       100100
+                       000000"""
+    cookie = [[int(chip) for chip in row.strip()] for row in cookie_string.split('\n')]
+    print(list(main(cookie, choco_chip)))
